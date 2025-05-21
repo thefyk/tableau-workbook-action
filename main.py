3a01a126011dfab08bbc68ed0d4f442b3e1ae51d
thefyk <michael.fyk@vizio.com>
2025-05-21 10:50:57 -0600

    Prints user

--- main.py
+++ main.py
@@ -87,6 +87,7 @@ def submit_workbook(workbook_schema, file_path, env):
     environment = os.environ['ENVIRONMENT']
     user = os.environ['USER']
 
+    print(user)
     environment_projects = {'stage': 'Stage','prod': 'Prod'}
     environment_project = environment_projects.get(environment, user) 
 
