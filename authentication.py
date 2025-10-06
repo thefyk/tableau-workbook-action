1473c081a5ac0fe176a7a3e02111f191b88ec62e
thefyk <michael.fyk@vizio.com>
2025-10-06 16:06:50 -0600

    Update authentication

--- authentication.py
+++ authentication.py
@@ -3,11 +3,23 @@ from tableauserverclient import ConnectionCredentials, ConnectionItem
 
 def get_tableau_connection(connection_name):
     host = os.environ[f'CONNECTIONS_{connection_name}_HOST']
-    user = os.environ[f'CONNECTIONS_{connection_name}_USER']
-    password = os.environ[f'CONNECTIONS_{connection_name}_PASSWORD']
+    user = os.environ.get(f'CONNECTIONS_{connection_name}_USER')
+    password = os.environ.get(f'CONNECTIONS_{connection_name}_PASSWORD')
 
-    connection = ConnectionItem()
-    connection.server_address = host
-    connection.connection_credentials = ConnectionCredentials(user, password, True)
+    if connection_name.lower() == 'snowflake':
+        connection = ConnectionItem()
+        connection.server_address = host
+        connection.connection_credentials = ConnectionCredentials(user, password, True)
+
+    if connection_name.lower() == 'databricks':
+        http_path = os.environ[f'CONNECTIONS_{connection_name}_HTTP_PATH']
+
+        connection = ConnectionItem()
+        connection.server_address = host
+        connection.port = 443
+        connection.connection_type = 'databricks'
+        connection.username = user
+        connection.password = password
+        connection.set_attribute('httpPath', http_path)
 
     return connection
