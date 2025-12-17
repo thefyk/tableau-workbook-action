de497bb7158e7167caff32b872b1bed3f71b3ce5
thefyk <michael.fyk@vizio.com>
2025-12-16 18:51:07 -0700

    Showtabs true

--- tableau_api.py
+++ tableau_api.py
@@ -174,7 +174,7 @@ class TableauApi:
         new_workbook = TSC.WorkbookItem(name = name, project_id = project_id, show_tabs=show_tabs)
         new_workbook.showTabs = True
 
-        new_workbook = server.workbooks.publish(new_workbook, file_path, TSC.Server.PublishMode.Overwrite, hidden_views=hidden_views)
+        new_workbook = server.workbooks.publish(new_workbook, file_path, TSC.Server.PublishMode.Overwrite, hidden_views=hidden_views, show_tabs=True)
 
         print('Updating Workbook to Show Tabs')
 
