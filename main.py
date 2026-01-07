5e5c46c3f6409d2512e5c30ac0915980237472a5
thefyk <michael.fyk@vizio.com>
2026-01-06 19:41:07 -0700

    Update schema definition

--- main.py
+++ main.py
@@ -39,14 +39,12 @@ class TableauWorkbookError(Exception):
 def get_full_schema(project_dir):
     logging.info(project_dir)
     from mergedeep import merge, Strategy
-    full_schema = None
     for schema_file in Path(project_dir).glob("**/*.yml"):
         schema = yaml.full_load(schema_file.open())
         logging.info(str(schema))
-        full_schema = merge(full_schema, schema, strategy=Strategy.ADDITIVE) if full_schema is not None else schema
 
     new_schema = dict({'workbooks':dict()})
-    for value in full_schema['workbooks']:
+    for value in schema['workbooks']:
         new_schema['workbooks'][value['file_path']] = value
 
     return new_schema
